0f2031a86264f8848cd14c34987f4b74a217de7b
sojicb <bogoljub.sojic987@gmail.com>
2024-02-06 20:53:27 +0100

    feat: add products controller

---
+++ WWDemo.Api/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WWDemo.Application.Products.Commands.AddProduct;
+
+namespace WWDemo.Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+        public ProductsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+		public async Task<IActionResult> AddProduct()
+		{
+			var result = await _mediator.Send(new AddProductCommand());
+
+			return Ok();
+		}
+	}
+}
