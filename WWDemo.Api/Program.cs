bc8b4e51169365a035d6ca5245205334a9421f4f
sojicb <bogoljub.sojic987@gmail.com>
2024-02-06 20:46:04 +0100

    feat: add application layer, add mediatr

--- WWDemo.Api/Program.cs
+++ WWDemo.Api/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Npgsql;
+using WWDemo.Application.Products.Commands.AddProduct;
 using WWDemo.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(AddProductCommand).Assembly));
 
 var connString = ConnectionStringProvider.GetConnectionString(builder.Configuration);
 builder.Services.AddDbContext<ApiDbContext>(options =>
