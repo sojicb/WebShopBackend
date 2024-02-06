7d3b216dd2d6971364b13848f8eb3eed9d666240
sojicb <bogoljub.sojic987@gmail.com>
2024-02-06 20:37:04 +0100

    feat: add product configuration

---
+++ WWDemo.Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WWDemo.Models;
+
+namespace WWDemo.Data.Configurations
+{
+	public class ProductConfiguration : IEntityTypeConfiguration<Product>
+	{
+		public void Configure(EntityTypeBuilder<Product> builder)
+		{
+			builder.HasIndex(x => x.SerialNumber);
+		}
+	}
+}
--- WWDemo.Data/Migrations/20240206193602_Add_Index_ProductsSerialNumber.Designer.cs
+++ WWDemo.Data/Migrations/20240206193602_Add_Index_ProductsSerialNumber.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WWDemo.Data;
@@ -11,9 +12,11 @@ using WWDemo.Data;
 namespace WWDemo.Data.Migrations
 {
     [DbContext(typeof(ApiDbContext))]
-    partial class ApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240206193602_Add_Index_ProductsSerialNumber")]
+    partial class Add_Index_ProductsSerialNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
