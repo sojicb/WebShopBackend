8ae2469bea17a33108f07df3ae724af4a5166dca
sojicb <bogoljub.sojic987@gmail.com>
2024-02-07 20:29:33 +0100

    feat add crud methods

--- WWDemo.Api/Controllers/ProductsController.cs
+++ WWDemo.Api/Controllers/ProductsController.cs
@@ -4,7 +4,7 @@ using WWDemo.Application.Products.Commands.AddProduct;
 
 namespace WWDemo.Api.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("[controller]")]
 	[ApiController]
 	public class ProductsController : ControllerBase
 	{
@@ -17,6 +17,30 @@ namespace WWDemo.Api.Controllers
 
         [HttpPost]
 		public async Task<IActionResult> AddProduct()
+		{
+			var result = await _mediator.Send(new AddProductCommand());
+
+			return Ok();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllProducts()
+		{
+			var result = await _mediator.Send(new AddProductCommand());
+
+			return Ok();
+		}
+
+		[HttpGet("serial-number")]
+		public async Task<IActionResult> GetProductBySerialNumber(int serialNumber)
+		{
+			var result = await _mediator.Send(new AddProductCommand());
+
+			return Ok();
+		}
+
+		[HttpDelete]
+		public async Task<IActionResult> DeleteProduct()
 		{
 			var result = await _mediator.Send(new AddProductCommand());
 
