a739123bf24b95c6797b6b3ffe3d7018d1c5e6f9
sojicb <bogoljub.sojic987@gmail.com>
2024-02-07 20:22:19 +0100

    feat: add app layer

---
+++ WWDemo.Application/Products/Commands/AddProduct/AddProductCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using WWDemo.Application.DTOs;
+
+namespace WWDemo.Application.Products.Commands.AddProduct
+{
+	public class AddProductCommand : IRequest<ProductRepresentation>
+	{
+	}
+}
